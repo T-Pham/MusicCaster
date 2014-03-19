8ee7aa5f700e1715bdfa7c6bd3752827b40665f7
Thanh Pham <thanh.pham@eastagile.com>
2014-03-19 19:33:14 +0700

    Initial Commit

---
+++ MusicCaster/TPMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TPMAppDelegate.h
+//  MusicCaster
+//
+//  Created by East Agile on 3/19/14.
+//  Copyright (c) 2014 East Agile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MusicCaster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusicCaster
+//
+//  Created by East Agile on 3/19/14.
+//  Copyright (c) 2014 East Agile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPMAppDelegate class]));
+  }
+}
